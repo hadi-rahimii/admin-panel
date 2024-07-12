6b0f8044df8103a6c82f37fa1797be302d1efdab
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-07-12 18:11:57 +0330

    show Topbar in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css'
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import "./App.css";
 function App() {
-
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-    
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
